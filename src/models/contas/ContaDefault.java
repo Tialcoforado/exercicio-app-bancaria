73c0445454a486d8d2cf31eb88a9d5ab4a6b704a
Tiago Alcoforado Coutinho <tipeco@gmail.com>
2022-06-22 20:30:23 -0300

    Add construtor ContaDefault

--- src/models/contas/ContaDefault.java
+++ src/models/contas/ContaDefault.java
@@ -3,8 +3,35 @@ import java.math.BigDecimal;
 
 import src.models.clientes.Cliente;
 
-public class ContaDefault {
+public class ContaDefault implements Conta {
     protected final Cliente cliente;
     protected BigDecimal saldo;
     protected final TipoConta tipoConta;
+    
+    protected ContaDefault(Cliente cliente, TipoConta tipoConta){
+        this.cliente = cliente;
+        this.tipoConta = tipoConta;
+        this.saldo = BigDecimal.ZERO;
+    }
+
+    @Override
+    public BigDecimal sacar(BigDecimal valor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public BigDecimal depositar(BigDecimal valor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public BigDecimal transferir(BigDecimal valor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public String consultarSaldo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
